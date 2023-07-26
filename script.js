a62548b1c1da5eed241d907b1ec0f2eacb2aa3fb
VARUN NAIR <137203305+NairVarun1@users.noreply.github.com>
2023-07-26 11:22:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let displayValue=''
+
+function appendToDisplay(value) {
+    displayValue += value;
+    document.getElementById('display').value = displayValue;
+    }
+    
+function clearDisplay() {
+    displayValue = '';
+    document.getElementById('display').value = '';
+}
+    
+function calculate() {
+    try {
+         displayValue = eval(displayValue);
+         document.getElementById('display').value = displayValue;
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
